42e61fa0e6539dfa88754ad77d93032bcb7b38d8
CelestinFernandes <celestin.fernandes21@st.niituniversity.in>
2024-11-06 03:05:38 +0530

    SAC-8 Add routing to main pages

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,5 +1,18 @@
-import React from 'react'
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Prediction from "./pages/Prediction";
+import SignUp from "./pages/SignUp";
+import SignIn from "./pages/SignIn";
 
 export default function App() {
-  return <h1 className='text-red-300'>App</h1>;
+  return <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/prediction" element={<Prediction />} />
+        <Route path="/signin" element={<SignIn />} />
+        <Route path="/signup" element={<SignUp />} />
+        <Route path="/about" element={<About />} />
+      </Routes>
+      </BrowserRouter>;
 }
