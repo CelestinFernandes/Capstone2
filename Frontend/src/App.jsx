4af9260608c5d6ef254f39abd3e15ed9dc50db60
CelestinFernandes <celestin.fernandes21@st.niituniversity.in>
2024-11-27 16:46:22 +0530

    SAC-67 Fix bug in App component

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React from "react";
 import Home from "./pages/Home";
 import About from "./pages/About";
 import Prediction from "./pages/Prediction";
 import SignUp from "./pages/SignUp";
 import SignIn from "./pages/SignIn";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
 export default function App() {
-  return <BrowserRouter>
+  return ( 
+  <BrowserRouter>
+  <Header />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/prediction" element={<Prediction />} />
@@ -14,5 +19,7 @@ export default function App() {
         <Route path="/signup" element={<SignUp />} />
         <Route path="/about" element={<About />} />
       </Routes>
-      </BrowserRouter>;
+      <Footer />  
+      </BrowserRouter>
+  );
 }
